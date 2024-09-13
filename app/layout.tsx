13c61f6550a97cb56abf77ad7fe99e3abbb3d149
Leben4164 <soft4164@gmail.com>
2024-09-13 16:21:37 +0900

    first commit

---
+++ app/layout.tsx
@@ -0,0 +1,20 @@
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "코어 출석",
+  description: "",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body>
+        {children}
+      </body>
+    </html>
+  );
+}
---
+++ components/attendance.tsx
@@ -0,0 +1,11 @@
+import Pocketbase from "pocketbase"
+
+export default function Attendance() {
+    const pb = new Pocketbase('http://localhost:8090')
+
+    return(
+        <div>
+            
+        </div>
+    )
+}
