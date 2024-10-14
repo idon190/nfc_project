14379f9d4e6f1c7288d369aac320a9951ed36412
Leben4164 <soft4164@gmail.com>
2024-10-14 16:13:06 +0900

    route 수정

--- app/request/route.ts
+++ app/request/route.ts
@@ -1,7 +1,27 @@
-interface uidProps{
-    searchParams : {uid?: string}
-}
 
-export async function Post({ searchParams } : uidProps ) {
-    return new Response(`${searchParams}`)
+import PocketBase from 'pocketbase'
+
+export async function Post(request: Request) {
+    const pb = new PocketBase(process.env.NEXT_PUBLIC_POCKETBASE_URL);
+    const uid = await request.json();
+    console.log(uid)
+
+    try {
+        const record = await pb.collection('students').getFirstListItem(`uid="${uid}"`)
+        pb.collection('students').update(record.id, {
+            attendance: true,
+            attendanceTime: new Date().toISOString
+        })
+    } catch (error) {
+        console.error(error);
+    }
+
+
+
+    return new Response(JSON.stringify({}), {
+        headers: {
+            "Content-Type": "application/json",
+        },
+        status: 201,
+    });
 }
